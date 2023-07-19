bdc1a1f5d0b5ed9d68c2970f8a9d72f96f09e4c0
Vanessa <vanessambeattie@gmail.com>
2023-07-18 19:44:17 -0500

    Shuffle and find a match working

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spre
 
 //Using null:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/null
+
+//setTimeOut:
+https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
+
+//alert():
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+//aAdding pictures to my script.js:
+https://code.visualstudio.com/docs/languages/markdown#:~:text=Inserting%20images%20and%20links%20to%20files&text=You%20can%20also%20insert%20images,inserted%20when%20you%20drop%20it.
+
+//toggle():
+https://www.w3schools.com/howto/howto_js_toggle_class.asp
--- script.js
+++ script.js
@@ -1,49 +1,138 @@
-//Creating picture's cointainers/arrays
-const frameContainer = document.querySelector(".frames");
-const pictures = ['bear', 'monkey', 'giraffe', 'lion', 'hypo', 'pinguin', 'dear', 'pig'];
-//..spread operator that replicates twice pictures array to prevent changes to going back to original array
-const pictureChoiceList = [...pictures, ...pictures];
-const pictureCount = pictureChoiceList.length;
+//*Pseudocoding
+//*Creating Picture Collection
+//Selecting div frames from html
+const frameContainer = document.querySelector('.frames');
 
-console.log(pictureChoiceList);
+// const frameContainer = [...document.querySelector('.frame')];
+const cardChosen = [];
+const cardChosenId = [];
+const cardsWon = [];
 
+let revealedPictureCount = 0;
+//null has a falsy value for boolean. Used for intentional absence of an object value
+let clickedFirstPicture = null;
+let clickedSecodPicture = false;
+//Creating my Picture collection
+const picturesCollection = ["Dear.jpg", "Bear.jpg", "Giraffe.png", "Hypo.jpg", 
+"Lion.jpg", "Monkey.jpg", "Pig.jpg", "Pinguin.jpg",
+"Dear.jpg", "Bear.jpg", "Giraffe.png", "Hypo.jpg", 
+"Lion.jpg", "Monkey.jpg", "Pig.jpg", "Pinguin.jpg"];
 
-//*Pseudocode:
-//*Initiating game:
-// let revealedPictureCount = 0;
-// //null has a falsy value for boolean. Used for intentional absence of an object value
-// let clickedFirstPicture = null;
-// let clickedSecodPicture = false;
-
-//*Creating frames
-function createFrame(picture) {
-    const element = document.createElement("div");
-
-    element.classList.add("frame");
-    element.setAttribute("picture-data", picture);
-
-    return element;
-
-}
-
+// const tempPicCollection = [...picturesCollection]
+console.log(picturesCollection);
 
 //*Shuffle cards
-for (let i = 0; i < pictureCount; i++) {
-    //selects pictures randomly through elemnts of array pictureChoiceList
-    const shuffleCards = Math.floor(Math.random() * pictureChoiceList.length)
-    const picture = pictureChoiceList[shuffleCards];
-    const frame = createFrame(picture);
-    console.log(picture);
 
-    //to select max 2 pictures of the same. It removes one picture of pictureChoiceList array
-    pictureChoiceList.splice(shuffleCards, 1);
-    frameContainer.appendChild(frame);
+picturesCollection.sort(() => 0.5 - Math.random())
+//!Need to fix that's taking more than 2 pictures of the same
 
-    console.log(picture);
+// const shuffledPictures = [];
+// for (picture in picturesCollection) {
+//     // for (let i = 0; i < picturesCollection.length; i++) {
+//         const shuffleIndex = Math.floor(Math.random() * picturesCollection.length);
+        
+        
+//         shuffledPictures.push(picturesCollection[shuffleIndex]);   
+        
+//         // picturesCollection.splice(shuffleIndex, 1)// tried this but reduces array to 8 elements
+        
+//     }
+    // console.log(shuffledPictures);
+    
+    //*Creating Frames
+    createFrame(picturesCollection);// changed frm Picture collection to shuffled picture but it's selecting more than 2 of each
+    function createFrame(pictures) {
+        for (let i = 0; i < pictures.length; i++) {
+            const element = document.createElement("div");
+            // creating class list
+            element.classList.add('frame');
+            // element.setAttribute('data-revealed', "false")//dont need it
+            // element.setAttribute('data-pictures', pictures)//dont need it
+            //formating my pictures size to fit frame  
+            element.style.backgroundImage = `url(${pictures[i]})`
+            element.style.backgroundSize = "50px 100px"
+            //!not sure if line below is doing anything
+            element.setAttribute('src', './Blank.png') //!this line is not dpong anything selecting blank card
+            element.setAttribute('data-id', pictures[i]);
+            //adding event listener to each card
+            // element.addEventListener('click', () {
+
+
+            // }) //it will call a flivcard function
+            frameContainer.appendChild(element)
+// }
+// }
+
+// // //*Adding EventListener
+// for (frame of picturesCollection){
+//     frame.addEventListener('click', viewCard)
+// }
+
+// function viewCard() {
+//     //If class already exists in the element's list of classes, it will not add the class again. toggle(): Toggles the existence of a class in an element's list of ...
+//     element.classList.toggle("open");
+//     element.classList.toggle("show");
+//     element.classList.toggle("disabled");
+    
+// }
+
+
+
+// frameContainer.addEventListener('click', flipCard() {
+    // frameContainer.addEventListener("click", () => {
+    //     const flipped = element.getAttribute("flipped-data");
+        
+    //     //if player gets a wrong match, returns and doesnt do anything
+    //     if (clickedSecodPicture || flipped === "true" || element === clickedFirstPicture) { //avpids clicking on the same tile twice
+    //         return;
+    //     }
+    //     element.style.backgroundImage = picture;
+
+    //     //*if first picture was selected
+    //     if (!clickedFirstPicture) {
+    //         clickedFirstPicture = element;
+
+    //         return;
+    //     }
+// })
 }
-//Pseudocoding
-//create frames
-//shuffle cards
+}
+
+//*Checking for Matches
+function checkForMatches() {
+    const cards = document.querySelectorAll('data-id');
+    const optionOneId = cardChosenId[0];
+    const optionTwoId = cardChosenId [1];
+    if (cardChosen[0] === cardChosen[1]){
+        alert('You found a match!');
+        cardsWon.push(cardChosen);
+    }   else {
+        cards[optionOneId].setAttribute('src', 'Blank.png')  
+        cards[optionTwoId].setAttribute('src', 'Blank.png')  
+        alert('Try again')
+    }
+    const cardChosen = [];
+    const cardChosenId = [];
+    if (cardsWon.length === picturesCollection.length) {
+        alert('Congratulations')
+    }
+}
+
+
+// //*Flipping Cards
+
+// function flipCard() {
+//     const cardId = this.getAttribute('data-id');
+//     cardChosen.push(picturesCollection[cardId]);
+//     cardChosenId.push(cardId);
+//     this.setAttribute('src', picturesCollection[cardId])//!I might not need this line
+//     if (cardChosen.length === 2) {
+//         setTimeout(checkForMatches, 500); //it will give 5 secods for second picture to be selected
+//     }
+// }
+
+//*User clicks on 1st card
+
 //connect cards to frame
 //user clicks on 1st card
 //1st card flips
@@ -53,7 +142,4 @@ for (let i = 0; i < pictureCount; i++) {
 //if cards are not the same waiting 3s and, both cards flip back
 //if all cards are flipped, you won the game
 //if all cards are not flipped, you keep playing
-//put button to quit playing game midway???
-
-
-
+//put button to quit playing game midway??
